5ad4e30e2cf64990f46e12e268b0288f154e0b7c
alspyro <spyropoulos.alexandre@gmail.com>
2025-09-09 18:51:40 +0300

    Update Workaround .sh

--- Workaround .sh
+++ Workaround .sh
@@ -1,9 +1,9 @@
 if ! diff -q <(tail -n +2 $properties_file) <(tail -n +2 $properties_template) >/dev/null; then
-        touch /cdr/COS/work/customImplementations/BQApps/BoxApp/properties/LOG/workaround_properties_file_$dd_stamp.log
-        echo "$dd_stamp: The file has less than 7 lines. The workaround will be performed in 30 mins" >> /cdr/COS/work/customImplementations/BQApps/BoxApp/properties/LOG/workaround_properties_file_$dd_stamp.log
+        touch file_$dd_stamp.log
+        echo "$dd_stamp: The file has less than 7 lines. The workaround will be performed in 30 mins" >> file_$dd_stamp.log
         sleep 30m
         dd_stamp_now=`date +%Y%m%d-%H.%M.%S`
-        echo "$dd_stamp_now: Workaround is starting" >> /cdr/COS/work/customImplementations/BQApps/BoxApp/properties/LOG/workaround_properties_file_$dd_stamp.log
+        echo "$dd_stamp_now: Workaround is starting" >> file_$dd_stamp.log
 
         first_line=$(head -n 1 $properties_file)
 
@@ -14,7 +14,7 @@ if ! diff -q <(tail -n +2 $properties_file) <(tail -n +2 $properties_template) >
         sed -i "1s/.*/$first_line/" $properties_file
         sed '2d' $properties_file >> temp.properties
         mv temp.properties $properties_file
-        echo "Workaround is completed!" >> /cdr/COS/work/customImplementations/BQApps/BoxApp/properties/LOG/workaround_properties_file_$dd_stamp.log
+        echo "Workaround is completed!" >> file_$dd_stamp.log
 
 else
         echo "The file is ok."
